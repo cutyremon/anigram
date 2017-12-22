1fdd1c1f4c40c6ef83d5b068bc5025fc06a864bf
Nguyen Ngoc Dong <cutyremon@gmail.com>
2017-12-23 01:40:04 +0700

    i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,30 @@
 class ApplicationController < ActionController::Base
-	protect_from_forgery with: :exception
-	include SessionsHelper
+  before_filter :set_i18n_locale
+  protect_from_forgery with: :exception
+  include SessionsHelper
 
-	private
+  private
 
-	# Confirms a logged-in user.
-		def logged_in_user
-			unless logged_in?
-				store_location
-				flash[:danger] = "Please log in"
-				redirect_to login_url
-			end
-		end
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end
+  end
+
+  def set_i18n_locale
+    if params[:locale]
+      if I18n.available_locales.include?(params[:locale].to_sym)
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] = params[:locale] + ' is not supported'
+      end
+    end
+  end
+
+  def default_url_options
+    {:locale => I18n.locale}
+  end
 end
